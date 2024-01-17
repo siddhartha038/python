bab0f66db4ce21f67cf6c00fd61a4099cd93ebdd
Siddhartha Singh <siddhartha038>
2024-01-17 10:06:46 +0530

    update

---
+++ ifelse.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your age:"))
+print("The age entered by you is:" , a)
+
+if(a>=18):
+    print("You are eligibe for driving")
+
+else:
+    print("You are not eligible for driving")
---
+++ stringoperation.py
@@ -0,0 +1,9 @@
+a = "Siddhartha Singh "
+print(a)
+print(len(a))
+print(a.upper)
+print(a.lower)
+print(a.capitalize)
+print(a.count("Siddhartha"))
+b = "Siddhartha is studying in csjmu kanpur"
+print(b.find("csjmu"))
