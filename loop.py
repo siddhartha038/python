f96f914c5e8714b16d08b26fc1ad363287d001eb
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-01-18 22:16:47 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,13 @@
+colors = ["Red , Yellow , Blue , Green , Purple"]
+for color in colors:
+      print(color)
+      
+      
+for k in range(20):
+    print(k)
+    
+count = 10
+while(count>0):
+    print(count) 
+
+    
