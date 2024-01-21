c1bf4d3b500d9cc01f8af05a1473c8d789144058
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-01-21 10:44:53 +0530

    Add files via upload

---
+++ breakandcontinue.py
@@ -0,0 +1,6 @@
+for i in range(11):
+    if(i==10):
+        break
+    print("5 *", i+1 ,"=" , 5*(i+1))
+    
+print("Loop is ended")
---
+++ continue.py
@@ -0,0 +1,8 @@
+for i in range(12):
+    if(i==10):
+     print("Skip the iteration")  
+     continue        
+    print("5 *", i ,"=" , 5*(i))
+    
+    
+    
