054c9b247a20320a2c2f880065ff809450cc0090
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-01-25 14:19:15 +0530

    Add files via upload

---
+++ defaultargument.py
@@ -0,0 +1,6 @@
+def average(a=5,b=4):
+    print("The average of both the numbers is:", (a+b)/2)
+    
+average(8)
+
+print("A default value of the functions will be taken but the value we assign later will have the priority")
---
+++ funarguments.py
@@ -0,0 +1,4 @@
+def average(a,b):
+    print("The average of both the numbers is:", (a+b)/2)
+    
+average(4,8)
