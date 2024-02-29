89a65298d1ba0055e6337156c67aba49a1d58c49
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-02-29 20:30:54 +0530

    Add files via upload

---
+++ tuplesmethods.py
@@ -0,0 +1,11 @@
+countries = ("India", "Australia", "USA", "China", "Brazil")
+print(countries)
+temp=(list(countries))
+temp.append("Russia")
+temp.pop(3)
+countries =tuple(temp)
+print(countries)
+
+countries2 = ("England", "Germany", "France")
+totalcountries =  countries + countries2
+print(len(totalcountries), totalcountries)
