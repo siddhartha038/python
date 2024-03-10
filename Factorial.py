2031177ba65291bcff0b88762bfd3544fdab3bba
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-03-10 20:09:29 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial cannot be found for negative numbers.")
+elif num == 0:
+    print("Factorial of 0 is 1.")
+else:
+    print("Factorial of", num, "is", factorial(num))
---
+++ evenodd.py
@@ -0,0 +1,8 @@
+# Taking input from the user
+num = int(input("Enter a number: "))
+
+# Checking if the number is even or odd
+if num % 2 == 0:
+    print(num, "is even.")
+else:
+    print(num, "is odd.")
