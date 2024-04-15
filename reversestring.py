6398c18d5fa86f84cdef14712acc83a117db0aa9
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-04-15 22:33:13 +0530

    Add files via upload

---
+++ reversestring.py
@@ -0,0 +1,7 @@
+def reverse_string(input_string):
+    return input_string[::-1]
+
+# Test the function
+input_string = input("Enter a string to reverse: ")
+reversed_string = reverse_string(input_string)
+print("Reversed string:", reversed_string)
