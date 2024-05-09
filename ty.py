15ecb455f9f5943abe7614b52f11f27813797f00
Siddhartha Singh <123258616+siddhartha038@users.noreply.github.com>
2024-05-09 08:22:34 +0530

    Add files via upload

---
+++ ty.py
@@ -0,0 +1,11 @@
+# Inserting different data type values into a tuple
+my_tuple = (10, 'hello', 3.14)
+
+# Unpacking the tuple into different variables
+num, greeting, pi_value = my_tuple
+
+# Displaying the unpacked values
+print("Unpacked values:")
+print("Number:", num)
+print("Greeting:", greeting)
+print("Pi value:", pi_value)
